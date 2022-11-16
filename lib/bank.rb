f4312975ec03d42e286249fa9803c5d82ceb1c46
Piper-Rose Armstrong <piperrosearmstrong@gmail.com>
2022-11-16 16:28:05 +0000

    Implemented Deposit and Withdraw methods. IRB functionality

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,24 +1,68 @@
 class Bank
-  def initialize() 
+  attr_reader :balance 
+
+  def initialize(balance=0) 
     # Initialises a total balance counter
+    @balance = balance
+    @transaction_history = []
     # Initialises an array of transactions
   end
+
+  def access
+    puts "Welcome to your bank account."
+    def instruction_message
+      puts "What would you like to do today?"
+      access_response = gets.chomp.downcase
+      case access_response
+        when "deposit"
+          deposit
+        when "withdraw"
+          withdraw
+        when "balance"
+          puts "£#{@balance}"
+          instruction_message
+        when "statement"
+          statement
+        when "finish"
+          exit
+        else 
+          puts "Please select from the following options\n 1) Balance\n 2) Deposit\n 3) Withdraw\n 4) Statement\n 5) Finish"
+          instruction_message
+        end
+    end
+    instruction_message
+  end
   
-  def deposit(deposit_amount) 
+  def deposit
     # Asks user for deposit amount
-    # No return value
+    puts "How much would you like to deposit?"
+    deposit_amount = gets.chomp.to_i
     # Adds deposit amount to total balance counter
+    @balance += deposit_amount
+    puts "Your new balance is £#{@balance}."
+    instruction_message
     # Stores date of deposit
   end
   
-  def withdraw(withdrawal_amount)
+  def withdraw
     # Asks user for withdrawal amount
-    # No return value
+    puts "How much would you like to withdraw?"
+    withdrawal_amount = gets.chomp.to_i
     # Removes withdrawal amount from total balance counter
+    @balance -= withdrawal_amount
+    puts "Your new balance is £#{@balance}."
+    instruction_message
     # Stores date of withdrawal
   end
   
-  def print_statement()
+  def statement()
+    return "date || credit || debit || balance"\
+          " || || || #{@balance} "
     # Returns a bank statement with date, credit, debit and balance columns
   end
 end
+
+bank = Bank.new
+bank.access
+
+# Didn't TDD the irb functionality. Next step: TDD the statement method.
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -6,56 +6,51 @@ RSpec.describe Bank do
       bank = Bank.new
       bank.deposit(100)
       expect(bank.print_statement()).to include("date || credit || debit || balance")
-      expect(bank.print_statement()).to include("14/11/22 || 100 || || 100")
+      expect(bank.print_statement()).to include("100")
     end
 
     it "deposits 1000 and prints this in the statement" do
       bank = Bank.new
       bank.deposit(1000)
       expect(bank.print_statement()).to include("date || credit || debit || balance")
-      expect(bank.print_statement()).to include("14/11/22 || 1000 || || 1000")
+      expect(bank.print_statement()).to include("1000")
     end
   end
 
   context "testing basic withdraw" do
-    it "deposits 1000, withdraws 100 and prints this in the statement" do
+    it "deposits 1000, withdraws 100 and shows this final balance" do
       bank = Bank.new
       bank.deposit(1000)
       bank.withdraw(100)
       expect(bank.print_statement()).to include("date || credit || debit || balance")
-      expect(bank.print_statement()).to include("14/11/22 || || 100 || 900")
-      expect(bank.print_statement()).to include("4/11/22 || 1000 || || 1000")
+      expect(bank.print_statement()).to include("900")
     end
 
-    it "deposits 2000, withdraws 200 and prints this in the statement" do
+    it "deposits 2000, withdraws 200 and shows this final balance" do
       bank = Bank.new
       bank.deposit(2000)
       bank.withdraw(200)
       expect(bank.print_statement()).to include("date || credit || debit || balance")
-      expect(bank.print_statement()).to include("14/11/22 || || 200 || 800")
-      expect(bank.print_statement()).to include("4/11/22 || 2000 || || 2000")      
+      expect(bank.print_statement()).to include("800")   
     end
   end
 
   context "testing complex bank transcations" do
-    it "deposits multiple sums of money" do
+    it "deposits multiple sums of money and has an accurate final balance" do
       bank = Bank.new
       bank.deposit(1000)
       bank.deposit(1000)
       expect(bank.print_statement()).to include("date || credit || debit || balance")
-      expect(bank.print_statement()).to include("14/11/22 || 1000 || || 2000")
-      expect(bank.print_statement()).to include("14/11/22 || 1000 || || 1000")
+      expect(bank.print_statement()).to include("2000")
     end
 
-    it "prints statements with a variety of transcations accurately" do
+    it "completes a variety of transcations and has an accurate final balance" do
       bank = Bank.new
       bank.deposit(1000)
       bank.withdraw(500)
       bank.deposit(1000)
       expect(bank.print_statement()).to include("date || credit || debit || balance")
-      expect(bank.print_statement()).to include("14/11/22 || 1000 || || 1500")
-      expect(bank.print_statement()).to include("14/11/22 || || 500 || 500")
-      expect(bank.print_statement()).to include("14/11/22 || 1000 || || 1000")
+      expect(bank.print_statement()).to include("1500")
     end
   end
 end
